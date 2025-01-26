ecbc0b749f22e15b392775be56c1050c9dd6b61f
Gleyton Lima <gleytonclima@gmail.com>
2025-01-26 00:42:25 -0400

    fix: ajustando editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/ProEpiDesenvolvimento/gds-ephem-docs",
+            "https://github.com/ProEpiDesenvolvimento/gds-ephem-docs/blob/main",
         },
         blog: {
           showReadingTime: true,
@@ -52,7 +52,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/ProEpiDesenvolvimento/gds-ephem-docs",
+            "https://github.com/ProEpiDesenvolvimento/gds-ephem-docs/blob/main",
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
